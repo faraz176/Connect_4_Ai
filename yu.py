0897dc259a23095647db7755abafd0f4fc158da4
Shuaib <shuaib.salar.ahmed@gmail.com>
2021-07-22 20:52:59 -0500

    Still in process of visual implementation, minimax still in progress

--- yu.py
+++ yu.py
@@ -1,8 +1,14 @@
 import random as r 
+import math
 
 class Board():
     def __init__(self, color1,):
+        cock = []
+        for i in range(1,7):
+            for z in range(0,7):
+                cock.append([])
         self.color_player_1 = color1
+        self.color_player_2 = ''
         self.turn = 0
         self.board_game = []
         self.moves_availiable = []
@@ -10,6 +16,7 @@ class Board():
         self.last_dropped = ''
         self.count_red_variable = 1
         self.count_yellow_variable = 1
+        self.looking_board = cock
 
 
     def board(self):
@@ -20,9 +27,43 @@ class Board():
                 coordinate = (i,z)
                 self.board_game.append(coordinate)
         for i in range(len(self.board_game)):
-            self.board_dict[self.board_game[i]] = ''        
+            self.board_dict[self.board_game[i]] = '' 
+
+
+    
+    # def show_board(self):
+    #     for i in range(1,7):
+    #         for z in range(0,7):
+    #             self.looking_board.append([])
+
+
+    def weird_board(self):
+
+            for i in range(1,7):
+                for z in range(0,7):
+                    self.looking_board.append([])
         
+        #Mapping
+            for i in range(len(self.board_game)):
+                self.looking_board[i] = self.board_game[i]
+
         
+            for i in range(len(self.board_game)):
+                if self.looking_board[i] == self.last_dropped:
+                    self.looking_board[i] = self.color_player_2
+
+
+            num_to_iterate = 0
+            num_to_end = 6 
+            for i in range(1,8):
+            
+                print(self.looking_board[num_to_iterate:num_to_end])
+                num_to_iterate += 6
+                num_to_end += 6 
+
+
+
+  
         
 
     def piece_drop(self, board_column):
@@ -35,6 +76,7 @@ class Board():
 
                 if self.board_dict[(board_column, i)] == '':
                     self.board_dict[(board_column, i)] = self.color_player_1
+                    self.color_player_2 = self.color_player_1
                     self.last_dropped = (board_column, i)
                     print(self.board_dict)
                     self.turn +=1
@@ -44,6 +86,7 @@ class Board():
                     color_2 = 'yellow'
                 if self.board_dict[(board_column, i)] == '':
                     self.board_dict[(board_column, i)] = color_2
+                    self.color_player_2 = color_2
                     self.last_dropped = (board_column, i)
                     print(self.board_dict)
                     self.turn = 0
@@ -85,7 +128,7 @@ class Board():
             if self.board_dict[coords_to_iterate[i]] == 'red':
                 count_red += 1
                 if count_red == 4:
-                    red_won = 'Red Wins!'
+                    red_won = 'red'
                     return red_won
 
             
@@ -100,7 +143,7 @@ class Board():
             if self.board_dict[coords_to_iterate[i]] == 'yellow':
                 count_yellow += 1
                 if count_yellow == 4:
-                    yellow_won = 'Yellow Wins!'
+                    yellow_won = 'yellow'
                     return yellow_won
             else:
                 count_yellow = 0 
@@ -113,7 +156,7 @@ class Board():
                     if self.board_dict[coords_to_check] == 'red':
                         count_red += 1
                         if count_red == 4:
-                            red_won = 'Red Wins!'
+                            red_won = 'red'
                             return red_won
                     else:
                         count_red = 0 
@@ -124,7 +167,7 @@ class Board():
                     if self.board_dict[coords_to_check] == 'yellow':
                         count_yellow += 1
                         if count_yellow == 4:
-                            yellow_won = 'Yellow Wins!'
+                            yellow_won = 'yellow'
                             return yellow_won
                     else:
                         count_yellow = 0 
@@ -137,7 +180,7 @@ class Board():
                 self.count_red_variable += 1
                 print("red: " + str(self.count_red_variable))
                 if self.count_red_variable == 4:
-                    red_won = 'Red Won'
+                    red_won = 'red'
                     return red_won
            
                 
@@ -150,15 +193,52 @@ class Board():
                 self.count_yellow_variable += 1
                 print("yellow: "+ str(self.count_yellow_variable))
                 if self.count_yellow_variable == 4:
-                    yellow_won = 'Yellow Won'
+                    yellow_won = 'yellow'
                     return yellow_won
             
             
         except KeyError:
             None
 
-    def minimax(self, depth, maximizingPlayer):
-        return None
+    def minimax(self):
+        if self.turn == 0:
+            maximizingPlayer = self.color_player_1
+        if self.turn == 1:
+            maximizingPlayer = 'yellow'
+        other_player = 'yellow'
+        
+        if self.win_checker() == other_player:
+            return {'position': None, 'score': 1 * (len(self.availiable_moves) + 1) if other_player == maximizingPlayer else -1 * (
+                        len(self.availiable_moves()) + 1)}
+
+        elif not self.availiable_moves():
+            return{'position': None, 'score':0}
+
+        if self.color_player_1 == maximizingPlayer:
+            best = {'position': None, 'score': -math.inf}
+        
+        else:
+            best = {'position': None, 'score': math.inf}
+
+        for possible_move in self.availiable_moves():
+            self.piece_drop(possible_move)
+            sim_score = self.minimax(self, other_player)
+
+
+            self.piece_drop[possible_move] = ' '
+            sim_score['position'] = possible_move
+
+            if self.color_player_1 == maximizingPlayer:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+        
+        return best
+
+      
 
 
             
@@ -172,36 +252,30 @@ class Board():
 new_game = Board('red')
 new_game.board()
 
-#turn = 0 
+turn = 0 
 x = None
 while x != 'done':
      
-    # if turn == 0:
-    #     board_column = int(input('Please input a column number from (0-6) '))
-    #     color = input('please input a color red or yellow ')
-    #     new_game.piece_drop(board_column, color)
-    #     turn += 1
-    
+    if turn == 0:
         board_column = int(input('Please input a column number from (0-6) '))
-        #color = input('please input a color red or yellow ')
         new_game.piece_drop(board_column)
-        f = new_game.availiable_moves()
-        print(f)
-        #turn += 1
-    # else:
-    #     choose = [0,1,2,3,4,5,6]
-    #     color_ai = 'yellow'
-    #     ai_column = r.choice(choose)
-    #     board_column = ai_column
-    #     color = color_ai
-    #     new_game.piece_drop(board_column, color)
-    #     turn = 0 
+        new_game.weird_board()
+        turn += 1
+
+             
+        
+    else:
+        board_column = int(input('Please input a column number from (0-6) '))
+        new_game.piece_drop(board_column)
+        new_game.weird_board()
+        turn = 0 
+    
 
 
-        vz = new_game.win_checker()
-        if vz != None:
-            print(vz)
-            x = 'done'
+    vz = new_game.win_checker()
+    if vz != None:
+        print(vz)
+        x = 'done'
     
 
 
